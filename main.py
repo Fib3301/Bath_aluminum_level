ab3d55ed2f821189e2e184854f4ff5f89e75a015
Sergey Kopytko <55785842+Fib3301@users.noreply.github.com>
2023-11-22 14:36:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,172 @@
+import cv2
+import numpy as np
+import threading
+
+from utils import *
+
+def main(video_path, contours, video_save_path, is_visualized=False):
+    cap = cv2.VideoCapture(video_path)
+    frame_per_second = cap.get(cv2.CAP_PROP_FPS)
+
+    if video_save_path:
+        fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+        out_shape = int(cap.get(3)), int(cap.get(4))
+        out = cv2.VideoWriter(video_save_path, fourcc, frame_per_second, out_shape, True)
+
+    frame_cur = 0
+    frame_step = frame_per_second
+
+    past_rows_colors = [[], [], []]
+    final_colors = [[], [], []]
+
+    while cap.isOpened():
+        is_next, frame = cap.read()
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_cur)
+        frame_cur += frame_step
+        if not is_next:
+            break
+
+        pool = []
+        for contour_index in contours.keys():
+            pool.append(
+                threading.Thread(
+                    target=check,
+                    args=(frame.copy(), contours, contour_index)
+                )
+            )
+        for t in pool:
+            t.start()
+        for t in pool:
+            t.join()
+
+        if is_visualized or video_save_path:
+            for contour_index, val in contours.items():
+                contour = val['contour']
+                flags = val['result']['flags']
+                flags_cell = val['result']['flags_cell']['is_warning_cell']
+                values = val['result']['values']
+                color = (0, 0, 255) if any(flags.values()) else (0, 255, 0)
+                test_pos_x = contour[:, 0].min()
+                test_pos_y = contour[:, 1].max()
+                text = f'{contour_index} | ' + ' | '.join([f'{k}: {v}' for k, v in flags.items()])
+                cv2.putText(
+                    frame, text, (test_pos_x, test_pos_y + 30),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1, color, 2
+                )
+                text = ' | '.join([f'{k}: {round(v, 4)}' for k, v in values.items()])
+                cv2.putText(
+                    frame, text, (test_pos_x, test_pos_y + 60),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1, color, 2
+                )
+                cv2.drawContours(frame, [contour], 0, color, 2)
+                height = np.max(contour[:, 1]) - np.min(contour[:, 1])
+                height_per_region = height // 4
+                regions = []
+                for i in range(4):
+                    y_min = np.min(contour[:, 1]) + i * height_per_region
+                    y_max = np.min(contour[:, 1]) + (i + 1) * height_per_region
+                    region_coordinates = np.array([[np.min(contour[:, 0]), y_min],
+                                                   [np.max(contour[:, 0]), y_min],
+                                                   [np.max(contour[:, 0]), y_max],
+                                                   [np.min(contour[:, 0]), y_max]])
+                    regions.append(region_coordinates)
+                rows_flags = np.array_split(flags_cell, 4)
+                colors = {'red': (0, 0, 255), 'yellow': (0, 255, 255), 'green': (0, 255, 0)}
+                color_rows = [None, None, None, None]
+                for i, region in enumerate(regions):
+                    region += np.array([[100, 200], [100, 200], [100, 200], [100, 200]])
+                    percentage = sum(rows_flags[i]) / len(rows_flags[i])
+                    if percentage >= 0.6:
+                        for j, color_row in enumerate(color_rows):
+                            if j >= i:
+                                color_rows[j] = colors['red']
+                    elif color_rows[i] != colors['red']:
+                        color_rows[i] = colors['green']
+
+                    if i == 3:
+                        past_rows_colors[contour_index].append(color_rows)
+                        if len(past_rows_colors[contour_index]) == 10:
+                            past_rows_colors[contour_index].pop(0)
+                            red_counts = [0, 0, 0, 0]
+                            green_counts = [0, 0, 0, 0]
+                            for sublist in past_rows_colors[contour_index]:
+                                for index, item in enumerate(sublist):
+                                    if item == (0, 0, 255):
+                                        red_counts[index] += 1
+                                    elif item == (0, 255, 0):
+                                        green_counts[index] += 1
+
+                            for r, g in zip(red_counts, green_counts):
+                                if r > g:
+                                    final_colors[contour_index].append(colors['red'])
+                                else:
+                                    final_colors[contour_index].append(colors['green'])
+                            final_colors[contour_index] = final_colors[contour_index][-4:]
+
+                    if final_colors[contour_index]:
+                        cv2.fillPoly(frame, [region], color=final_colors[contour_index][i])
+                        cv2.drawContours(frame, [region], 0, (255, 255, 255), 2)
+
+            if is_visualized:
+                cv2.imshow('Frame', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            if video_save_path:
+                out.write(frame)
+
+    cap.release()
+    if video_save_path:
+        out.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    contours = [
+        {
+            'contour': np.array([[1155, 660], [1412, 625], [1412, 677], [1162, 715]]),
+            'bounds': {
+                'mean': (200, 235),
+                'median': (200, 235),
+                'std': (0, 105),
+            },
+            'warnings': {
+                'mean': 0.55,
+                'median': 0.55,
+                'std': 1.05,
+            },
+        },
+        {
+            'contour': np.array([[685, 645], [980, 610], [980, 680], [710, 710]]),
+            'bounds': {
+                'mean': (200, 230),
+                'median': (200, 230),
+                'std': (0, 100),
+            },
+            'warnings': {
+                'mean': 0.6,
+                'median': 0.6,
+                'std': 1.1,
+            },
+        },
+        {
+            'contour': np.array([[262, 407], [492, 368], [497, 400], [267, 435]]),
+            'bounds': {
+                'mean': (140, 200),
+                'median': (140, 200),
+                'std': (0, 110),
+            },
+            'warnings': {
+                'mean': 1.6,
+                'median': 0.75,
+                'std': 1.8,
+            },
+        },
+    ]
+    contours = {i: v for i, v in enumerate(contours)}
+    main(
+        video_path='2023-07-12_03_49_11_03_55_00_4306a3db50ad28fc.mp4',
+        contours=contours,
+        is_visualized=True,
+        video_save_path='out_2023-07-12_03:49:11_03:55:00_4306a3db50ad28fc.mp4',
+    )
---
+++ utils.py
@@ -0,0 +1,236 @@
+import cv2
+import numpy as np
+
+def crop(img, pts):
+    mask = np.zeros(img.shape[:2], dtype=np.uint8)
+    cv2.drawContours(mask, [pts], -1, (255, 255, 255), -1, cv2.LINE_AA)
+    res = cv2.bitwise_and(img, img, mask=mask)
+    rect = cv2.boundingRect(pts)
+    cropped = res[rect[1]:rect[1] + rect[3], rect[0]:rect[0] + rect[2]]
+    return cropped
+
+def order_points(pts):
+    rect = np.zeros((4, 2), dtype="float32")
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    return rect
+
+def rotate(img, pts):
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(img, M, (maxWidth, maxHeight))
+    return warped
+
+def calibrate_frame(frame, target_mean, target_std):
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    mean_val = np.mean(gray_frame)
+    std_val = np.std(gray_frame)
+    contrast = target_std / std_val
+    brightness = target_mean - (mean_val * contrast)
+    adjusted_frame = cv2.convertScaleAbs(gray_frame, alpha=contrast, beta=brightness)
+    calibrated_frame = cv2.cvtColor(adjusted_frame, cv2.COLOR_GRAY2BGR)
+    return calibrated_frame
+
+def quantization(img, clusters=8, rounds=1):
+    samples = img.reshape(-1, 3).astype(np.float32)
+    compactness, labels, centers = cv2.kmeans(
+        samples, clusters, None,
+        (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10000, 0.0001),
+        rounds, cv2.KMEANS_PP_CENTERS
+    )
+    centers = np.uint8(centers)
+    res = centers[labels.flatten()]
+    return res.reshape(img.shape)
+
+def grid(img, cells_in_height, cells_in_width):
+    h, w = img.shape[:2]
+    d_h = h // cells_in_height
+    d_w = w // cells_in_width
+    tiles = {}
+    for i in range(0, h, d_h):
+        for j in range(0, w, d_w):
+            box = (i, j, i + d_h, j + d_w)
+            tile_img = img[box[0]:box[2], box[1]:box[3]]
+            tiles[(i, j)] = tile_img
+    return tiles
+
+def calculate_statistics_in_tiles(tiles):
+    stats_element = {}
+    for coords, tile_img in tiles.items():
+        mean_value = round(np.mean(tile_img), 2)
+        std_value = round(np.std(tile_img), 2)
+        median_value = round(np.median(tile_img), 2)
+        stats_element[coords] = mean_value, std_value, median_value
+    return stats_element
+
+def calculate_overall_statistics(stats_element):
+    all_mean_values, all_std_values, all_median_values, all_coords = [], [], [], []
+    for coords, (mean_value, std_value, median_value) in stats_element.items():
+        all_mean_values.append(mean_value)
+        all_std_values.append(std_value)
+        all_median_values.append(median_value)
+        all_coords.append(coords)
+
+    overall_mean = round(np.mean(all_mean_values), 2)
+    overall_std = round(np.std(all_std_values), 2)
+    overall_median = round(np.median(all_median_values), 2)
+
+    return {
+        'mean': overall_mean,
+        'std': overall_std,
+        'median': overall_median,
+        'all_mean': all_mean_values,
+        'all_std': all_std_values,
+        'all_median': all_median_values,
+        'all_coords': all_coords
+    }
+
+def detect_outliers_in_rows(stats_element):
+    grouped_rows = {}
+    for coord, values in stats_element.items():
+        x_coord = coord[0]
+        if x_coord not in grouped_rows:
+            grouped_rows[x_coord] = []
+        grouped_rows[x_coord].append((coord, values))
+
+    def find_and_normalize_outliers(row):
+        mean_values = [value[0] for _, value in row]
+        std_values = [value[1] for _, value in row]
+        median_values = [value[2] for _, value in row]
+        mean_mean = sum(mean_values) / len(mean_values)
+        mean_std = sum(std_values) / len(std_values)
+        mean_median = sum(median_values) / len(median_values)
+        outlier_coords, recession_coords = [], []
+
+        for coord, values in row:
+            if (
+                values[0] > (mean_mean + mean_std) * 1.5
+                or values[1] > (mean_std + mean_std) * 1.5
+                or values[2] > (mean_median + mean_std) * 1.5
+            ):
+                outlier_coords.append(coord)
+            elif (
+                values[0] < (mean_mean - mean_std) * 0.3
+                or values[1] < (mean_std - mean_std) * 0.3
+                or values[2] < (mean_median - mean_std) * 0.3
+            ):
+                recession_coords.append(coord)
+
+        for outlier_coord in outlier_coords:
+            outlier_mean, outlier_std, outlier_median = stats_element[outlier_coord]
+            stats_element[outlier_coord] = (mean_mean, mean_std, mean_median)
+            warning2(outlier_coord)
+
+        for recession_coord in recession_coords:
+            outlier_mean, outlier_std, outlier_median = stats_element[recession_coord]
+            stats_element[recession_coord] = (mean_mean, mean_std, mean_median)
+            warning3(recession_coord)
+
+    for row in grouped_rows.values():
+        find_and_normalize_outliers(row)
+
+    return stats_element
+
+def grid_visualiser(finally_statistics, bnds, wrns):
+    all_mean_val, all_std_val, all_median_val, all_coords, is_warning_cell = [], [], [], [], []
+
+    for element_all_mean_val, element_all_std_val, element_all_median_val in zip(finally_statistics['all_mean'],
+                                                                                 finally_statistics['all_std'],
+                                                                                 finally_statistics['all_median']):
+        element_all_mean_val = sf_cell(bnds, element_all_mean_val, 'mean')
+        element_all_std_val = sf_cell(bnds, element_all_std_val, 'std')
+        element_all_median_val = sf_cell(bnds, element_all_median_val, 'median')
+
+        all_mean_val.append(element_all_mean_val)
+        all_std_val.append(element_all_std_val)
+        all_median_val.append(element_all_median_val)
+        is_warning = any((
+            element_all_mean_val >= wrns['mean'],
+            element_all_std_val >= wrns['std'],
+            element_all_median_val >= wrns['median'],
+        ))
+        is_warning_cell.append(is_warning)
+    if any(is_warning_cell):
+        warning()
+    return {
+        'all_mean_val': all_mean_val,
+        'all_std_val': all_std_val,
+        'all_median_val': all_median_val,
+        'all_coords': all_coords,
+        'is_warning_cell': is_warning_cell,
+    }
+
+def sf(x, bnds, name):
+    return (x[name] - bnds[name][0]) / (bnds[name][1] - bnds[name][0])
+
+def sf_cell(bnds, val, name):
+    return (val - bnds[name][0]) / (bnds[name][1] - bnds[name][0])
+
+def warning():
+    print(f'[WARNING]: Exceeding the level')
+
+def warning2(outlier_coord):
+    print(f"[OUTLIER]:  in coordinate: {outlier_coord}")
+
+def warning3(outlier_coord):
+    print(f"[RECESSION]: in coordinate: {outlier_coord}")
+
+def check(img, cnts, cnt_idx):
+    cnt = cnts[cnt_idx]['contour']
+    bnds = cnts[cnt_idx]['bounds']
+    wrns = cnts[cnt_idx]['warnings']
+
+    rotated = rotate(img, cnt)
+    grided = grid(rotated, 3, 15)
+    statistics_grid = calculate_statistics_in_tiles(grided)
+    outliers_control = detect_outliers_in_rows(statistics_grid)
+    finally_statistics = calculate_overall_statistics(outliers_control)
+    cell_statistics = grid_visualiser(finally_statistics, bnds, wrns)
+
+    mean_val = sf(finally_statistics, bnds, "mean")
+    std_val = sf(finally_statistics, bnds, "std")
+    median_val = sf(finally_statistics, bnds, "median")
+
+    is_warning = any((
+        mean_val >= wrns['mean'],
+        std_val >= wrns['std'],
+        median_val >= wrns['median'],
+    ))
+    if is_warning:
+        warning()
+
+    cnts[cnt_idx]['result'] = {
+        'flags': {
+            'is_warning': is_warning,
+        },
+        'values': {
+            'mean': mean_val,
+            'std': std_val,
+            'median': median_val,
+        },
+        'values_cell': {
+            'all_mean': cell_statistics['all_mean_val'],
+            'all_std': cell_statistics['all_std_val'],
+            'all_median': cell_statistics['all_median_val'],
+            'all_coords': cell_statistics['all_coords'],
+        },
+        'flags_cell': {
+            'is_warning_cell': cell_statistics['is_warning_cell'],
+        }
+    }
